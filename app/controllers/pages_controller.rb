a21834e40360cf77ce8e396a37d079f69657d09e
Dareos Khalili <khalilidareos@gmail.com>
2021-05-14 17:16:48 +0200

    adds route, action and view for dahsboard, spits some data on the page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, except: [:dashboard]
 
   def home
   end
@@ -8,5 +8,11 @@ class PagesController < ApplicationController
     # TODO: order articles and exhibitions by date
     @articles = Article.all
     @exhibitions = Exhibition.all
+  end
+
+  def dashboard
+    @user = current_user
+    @articles = Article.all.where(user: current_user)
+    @exhibitions = Exhibition.all.where(user: current_user)
   end
 end
