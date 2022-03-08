5a8f49dc416c7052d9d6d3fbcabb70488c9fd8f5
Dareos Khalili <khalilidareos@gmail.com>
2022-03-08 20:16:22 +0100

    adds migrations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_rich_text :rich_body
+
+  # friendly Id for friendly URLS
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
